ba9480b192b271f0a09f4696f7c27b55e2079eef
= <tijereto63@gmail.com>
2018-05-23 13:21:56 -0400

    project works

--- app.js
+++ app.js
@@ -1,34 +1,11 @@
 
-//https://www.googleapis.com/auth/youtube
-//https://console.cloud.google.com/home/activity?project=my-project-youtube-203622
-
-/*$(document).ready(function () {
-
-    var $video = $('#video');
-    var url = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&key="
-    var key = "AIzaSyD_SbAZcuNTOgJq6mQ9WkPFHvjxUltJhC0";
-    $.getJSON(url + key, function(data){
-      console.log(data);
-    })
-});*/
-
-/*<iframe id="naruto" width="560" height="315" src="https://www.youtube.com/embed/wEWF2xh5E8s" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-    <iframe id="action" width="560" height="315" src="https://www.youtube.com/embed/aijFRVLD_j8" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-    <iframe id="happysong" width="560" height="315" src="https://www.youtube.com/embed/y6Sxv-sUYtM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-    <iframe id="cat" width="560" height="315" src="https://www.youtube.com/embed/DODLEX4zzLQ" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>*/
-
-
-
-
-
-
 var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 const videoIds = {
-  good: {
+  Good: {
     1980: {
       Spring: "wEWF2xh5E8s",
       Winter: "y6Sxv-sUYtM",
@@ -82,6 +59,11 @@ const videoIds = {
   sad: {}*/
 }};
 
+const mood = document.getElementById("mood").value;
+const year = document.getElementById('year').value;
+const station = document.getElementById('station').value;
+const youtube = document.getElementById("submit");
+
        var player;
       function onYouTubeIframeAPIReady() {
         player = new YT.Player('player', {
@@ -93,17 +75,16 @@ const videoIds = {
           }
         });
       }
-const youtube = document.getElementById("submit");
+
+
       function onPlayerReady(event) {
         document.getElementById('submit').addEventListener('click', function() {
-          player.loadVideoById({videoId: videoIds,
-                     suggestedQuality:'large'});
-          player.playVideo();1
+          player.loadVideoById({
+                    videoId:  videoIds[mood][year][station],
+                    startSeconds:1,
+                    suggestedQuality:'large'});
+          
         });
-        
-  if (youtube === videoIds[mood], [year], [station]) {
-      document.getElementById('player').style.display = "block";
-   }
       }
      
 
@@ -112,15 +93,10 @@ const youtube = document.getElementById("submit");
 /*youtube.onclick = function() {*/
   /*const answer = document.getElementById("mood", "song", "movie").value;
   */
-/*  const mood = document.getElementById("mood").value;
-  const year = document.getElementById('year').value;
-  const station = document.getElementById('station').value;
-  console.log();
-  
- */
-
 /*};*/
 //videoIds[mood][year][station];
 
-
+/*if (youtube === videoIds[mood], [year], [station]) {
+      document.getElementById('player').style.display = "block";
+   }*/
 
